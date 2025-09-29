bb4219cbf7cac2add50f57f3b6c521e192d935ed
Vinicius de Oliveira <82851863+vnzoliveira@users.noreply.github.com>
2025-09-28 21:42:59 -0300

    Revert "Refact: improve desafio2  (#5)" (#6)
    This reverts commit e683aa69f3afda81ccbfa2adbae0b77820bf23b0.

--- src/ui/pages/index.py
+++
@@ -1,8 +0,0 @@
-# src/ui/pages/index.py
-from dash import register_page, dcc
-
-register_page(__name__, path="/", title="Início")
-
-def layout():
-    # redireciona imediatamente para /desafio1
-    return dcc.Location(id="redir", href="/desafio1")
